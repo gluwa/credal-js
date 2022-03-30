f7b65e7997ea796b24ac28add9c16298ec0293a9
Jeremy Frank <jeremy.frank@gluwa.com>
2022-03-30 11:54:23 -0600

    add registerAddressAsync

--- src/extrinsics/register-address.ts
+++ src/extrinsics/register-address.ts
@@ -1,8 +1,10 @@
-import { ApiPromise } from '@polkadot/api';
-import { Blockchain } from '../model';
+import { ApiPromise, SubmittableResult } from '@polkadot/api';
+import { Address, Blockchain } from '../model';
 import { KeyringPair } from '@polkadot/keyring/types';
 import { handleTransaction } from '../utils';
 import { TxOnFail, TxOnSuccess } from '../types';
+import { AddressConverter } from '../transform';
+import { GenericEventData } from '@polkadot/types/';
 
 export const registerAddress = async (
     api: ApiPromise,
@@ -15,4 +17,34 @@ export const registerAddress = async (
     const unsubscribe: () => void = await api.tx.creditcoin
         .registerAddress(blockchain, externalAddress)
         .signAndSend(signer, { nonce: -1 }, (result) => handleTransaction(api, unsubscribe, result, onSuccess, onFail));
+};
+
+type RegisteredAddress = {
+    addressId: string;
+    address: Address;
+};
+
+const processRegisteredAddress = (api: ApiPromise, result: SubmittableResult): RegisteredAddress | undefined => {
+    const { events } = result;
+    const addressRegistered = events.find(({ event }) => event.method == 'AddressRegistered');
+
+    const getData = (data: GenericEventData) => {
+        const addressId = data[0].toString();
+        const address = AddressConverter.toModel(api.createType('PalletCreditcoinAddress', data[1]));
+        return { addressId, address }; //{ [addressId]: info }
+    };
+    return addressRegistered && getData(addressRegistered.event.data);
+};
+
+export const registerAddressAsync = async (
+    api: ApiPromise,
+    externalAddress: string,
+    blockchain: Blockchain,
+    signer: KeyringPair,
+) => {
+    return new Promise<RegisteredAddress | undefined>(async (resolve) => {
+        const onFail = () => resolve(undefined);
+        const onSuccess = (result: SubmittableResult) => resolve(processRegisteredAddress(api, result));
+        await registerAddress(api, externalAddress, blockchain, signer, onSuccess, onFail);
+    });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
 import { Keyring } from '@polkadot/api';
 import { creditcoinApi } from './connection';
-import { registerAddress } from './extrinsics/register-address';
+import { registerAddress, registerAddressAsync } from './extrinsics/register-address';
 
 const main = async () => {
-    const api = await creditcoinApi('wss://testnet.creditcoin.network');
+    const api = await creditcoinApi('ws://localhost:9944');
     const keyring = new Keyring({ type: 'sr25519' });
     const signer = keyring.addFromUri('//Alice');
 
-    await registerAddress(
-        api,
-        '0x1234567890123456789012345678901234567890',
-        'Ethereum',
-        signer,
-        () => api.disconnect(),
-        () => api.disconnect(),
-    );
+    const address = await registerAddressAsync(api, '0x1234567890123456789012345678901234564810', 'Ethereum', signer);
+    console.log(address);
 };
 
 main().catch(console.error);
