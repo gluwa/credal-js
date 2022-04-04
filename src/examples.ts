1dfa194d913b68b80ac5c535423aed1b4be489ee
Jeremy Frank <jeremy.frank@gluwa.com>
2022-04-04 12:17:48 -0600

    convert tslint to eslint

--- src/examples.ts
+++ src/examples.ts
@@ -1,16 +1,18 @@
 import { Keyring } from '@polkadot/api';
 import { creditcoinApi } from './connection';
 import { registerAddressAsync } from './extrinsics/register-address';
+import { Wallet } from 'ethers';
 
 const main = async () => {
     const api = await creditcoinApi('wss://testnet.creditcoin.network');
     const keyring = new Keyring({ type: 'sr25519' });
     const signer = keyring.addFromUri('//Alice');
 
-    const address = await registerAddressAsync(api, '0x12345678901234567890123456789d01234564810', 'Ethereum', signer);
+    const ethAddress = Wallet.createRandom().address;
+    const address = await registerAddressAsync(api, ethAddress, 'Ethereum', signer);
     console.log(address);
 
-    api.disconnect();
+    await api.disconnect();
 };
 
 main().catch(console.error);
