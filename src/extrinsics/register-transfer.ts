5eeb21509747dea1f94ebf625c5ff42a73e8c149
Jeremy Frank <jeremy.frank@gluwa.com>
2022-04-09 06:21:22 -0600

    add registerTransferAsync

--- src/extrinsics/register-transfer.ts
+++ src/extrinsics/register-transfer.ts
@@ -1,29 +1,63 @@
-import { ApiPromise } from '@polkadot/api';
-import { Blockchain, DealOrderId, TransferId, TransferKind } from '../model';
+import { ApiPromise, SubmittableResult } from '@polkadot/api';
+import { Blockchain, DealOrderId, Transfer, TransferId, TransferKind } from '../model';
+import { GenericEventData } from '@polkadot/types/';
 import { u8aConcat, u8aToU8a } from '@polkadot/util';
 import { blake2AsHex } from '@polkadot/util-crypto';
-import { createCreditcoinTransferKind } from 'credal-js/transforms';
+import { createCreditcoinTransferKind, createTransfer } from 'credal-js/transforms';
 import { KeyringPair } from '@polkadot/keyring/types';
-import { handleTransaction } from './common';
+import { handleTransaction, handleTransactionFailed } from './common';
 import { TxCallback } from '..';
 
-export const createTransferId = (blockchain: Blockchain, blockchainTxId: string): TransferId => {
+export type TransferEventKind = 'TransferRegistered' | 'TransferVerified' | 'TransferProcessed';
+export type TransferEvent = {
+    kind: TransferEventKind;
+    transferId: TransferId;
+    transfer: Transfer;
+};
+
+export const createTransferId = (blockchain: Blockchain, externalAddress: string) => {
     const blockchainBytes = Buffer.from(blockchain.toString().toLowerCase());
-    const key = u8aConcat(blockchainBytes, u8aToU8a(blockchainTxId));
+    const key = u8aConcat(blockchainBytes, u8aToU8a(externalAddress));
     return blake2AsHex(key);
 };
 
-export const registerFundingTransfer = async (
+export const registerTransfer = async (
     api: ApiPromise,
-    transferKind: TransferKind,
-    dealOrderId: DealOrderId,
-    blockchainTxId: string,
-    lender: KeyringPair,
+    externalAddress: string,
+    blockchain: Blockchain,
+    signer: KeyringPair,
     onSuccess: TxCallback,
     onFail: TxCallback,
 ) => {
-    const ccDealOrderId = api.createType('PalletCreditcoinDealOrderId', dealOrderId);
     const unsubscribe: () => void = await api.tx.creditcoin
-        .registerFundingTransfer(createCreditcoinTransferKind(api, transferKind), ccDealOrderId, blockchainTxId)
-        .signAndSend(lender, { nonce: -1 }, (result) => handleTransaction(api, unsubscribe, result, onSuccess, onFail));
+        .registerTransfer(blockchain, externalAddress)
+        .signAndSend(signer, { nonce: -1 }, (result) => handleTransaction(api, unsubscribe, result, onSuccess, onFail));
+};
+
+const processTransferEvent = (api: ApiPromise, result: SubmittableResult, kind: TransferEventKind): TransferEvent => {
+    const { events } = result;
+    const transferEvent = events.find(({ event }) => event.method === kind);
+    if (!transferEvent) throw new Error(`${kind} call returned invalid data`);
+
+    const getData = (data: GenericEventData) => {
+        const transferId = data[0].toString();
+        const transfer = createTransfer(api.createType('PalletCreditcoinTransfer', data[1]));
+        return { kind, transferId, transfer };
+    };
+
+    return getData(transferEvent.event.data);
+};
+
+export const registerTransferAsync = async (
+    api: ApiPromise,
+    externalAddress: string,
+    blockchain: Blockchain,
+    signer: KeyringPair,
+) => {
+    return new Promise<TransferEvent>((resolve, reject) => {
+        const onFail = (result: SubmittableResult) => reject(handleTransactionFailed(api, result));
+        const onSuccess = (result: SubmittableResult) =>
+            resolve(processTransferEvent(api, result, 'TransferRegistered'));
+        registerTransfer(api, externalAddress, blockchain, signer, onSuccess, onFail);
+    });
 };
